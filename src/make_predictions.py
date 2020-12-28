fba5fa5e89a67425cdba9c639166a09aa8a9f0b3
Melvin Wevers <melvinwevers@gmail.com>
2020-12-28 09:25:54 +0100

    add camera-ready manuscript

--- src/make_predictions.py
+++ src/make_predictions.py
@@ -1,1 +1,163 @@
+from PIL import Image
+from torch import optim, cuda
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import torch
+
+from IPython.display import set_matplotlib_formats
+from matplotlib import rc
+
+rc('font', **{'family': 'serif', 'serif': ['Palatino']})
+set_matplotlib_formats('retina', quality=100)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+train_on_gpu = cuda.is_available()
+print('Train on gpu: {}'.format(train_on_gpu))
+
+# def imshow(image):
+#     """Display image"""
+#     plt.figure(figsize=(6, 6))
+#     plt.imshow(image)
+#     plt.axis('off')
+#     plt.show()
+
+
+
+def imshow_tensor(image, ax=None, title=None):
+    """Imshow for Tensor."""
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    # Set the color channel as the third dimension
+    image = image.numpy().transpose((1, 2, 0))
+
+    # Reverse the preprocessing steps
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+
+    # Clip the image pixel values
+    image = np.clip(image, 0, 1)
+
+    ax.imshow(image)
+    plt.axis('off')
+
+    return ax, image
+
+
+def process_image(image_path):
+    """Process an image path into a PyTorch tensor"""
+
+    image = Image.open(image_path)
+    # Resize
+    img = image.resize((256, 256))
+
+    # Center crop
+    width = 256
+    height = 256
+    new_width = 224
+    new_height = 224
+
+    left = (width - new_width) / 2
+    top = (height - new_height) / 2
+    right = (width + new_width) / 2
+    bottom = (height + new_height) / 2
+    img = img.crop((left, top, right, bottom))
+
+    # Convert to numpy, transpose color dimension and normalize
+    img = np.array(img)
+    img = np.array(img)
+    if img.ndim == 3:
+        pass
+    else:
+        img = np.repeat(img[:, :, np.newaxis], 3, axis=2)
+    img = img.transpose((2, 0, 1)) / 256
+
+    #Standardization
+    means = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
+    stds = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
+
+    img = img - means
+    img = img / stds
+
+    img_tensor = torch.Tensor(img)
+
+    return img_tensor
+
+def random_test_image(img_dir, categories):
+    """Pick a random test image from specified dir"""
+    c = np.random.choice(categories)
+    root = img_dir + c + '/'
+    img_path = root + np.random.choice(os.listdir(root))
+    print(img_path)
+    return img_path
+
+
+def display_prediction(image_path, model, topk):
+    """Display image and preditions from model"""
+
+    # Get predictions
+    img, ps, classes, y_obs = predict(image_path, model, topk)
+    classes = [x[11:] for x in classes]
+    # Convert results to dataframe for plotting
+    result = pd.DataFrame({'p': ps}, index=classes)
+
+    # Show the image
+    plt.figure(figsize=(16, 5))
+    ax = plt.subplot(1, 2, 1)
+    ax, img = imshow_tensor(img, ax=ax)
+
+    # Set title to be the actual class
+    ax.set_title(y_obs, size=20)
+
+    ax = plt.subplot(1, 2, 2)
+    # Plot a bar plot of predictions
+    result.sort_values('p')['p'].plot.barh(color='blue', edgecolor='k', ax=ax)
+    plt.xlabel('Predicted Probability')
+    plt.tight_layout()
+
+
+def predict(image_path, model, topk=5):
+    """Make a prediction for an image using a trained model
+
+    Params
+    --------
+        image_path (str): filename of the image
+        model (PyTorch model): trained model for inference
+        topk (int): number of top predictions to return
+
+    Returns
+
+    """
+    real_class = image_path.split('/')[-2]
+
+    # Convert to pytorch tensor
+    img_tensor = process_image(image_path)
+
+    # Resize
+    if train_on_gpu:
+        img_tensor = img_tensor.view(1, 3, 224, 224).cuda()
+    else:
+        img_tensor = img_tensor.view(1, 3, 224, 224)
+
+    # Set to evaluation
+    with torch.no_grad():
+        model.eval()
+        # Model outputs log probabilities
+        out = model(img_tensor)
+        ps = torch.exp(out)
+
+        # Find the topk predictions
+        topk, topclass = ps.topk(topk, dim=1)
+
+        # Extract the actual classes and probabilities
+        top_classes = [
+            model.idx_to_class[class_] for class_ in topclass.cpu().numpy()[0]
+        ]
+        top_p = topk.cpu().numpy()[0]
+
+        return img_tensor.cpu().squeeze(), top_p, top_classes, real_turclass
 
